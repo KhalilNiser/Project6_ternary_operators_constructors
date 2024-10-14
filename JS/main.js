27a861aaa5f8b2db39ae9558960585776db6d695
KhalilNiser <kaliniser@gmail.com>
2024-10-14 12:04:05 -0700

    Project6 Updated

--- JS/main.js
+++ JS/main.js
@@ -1,3 +1,4 @@
+//              ---- TERNARY_OPERATOR ----
 //              ---- VOTING_AGE_COMPARISON_FUNCTION ----
 function voteableAgeComparison()
 {
@@ -6,7 +7,7 @@ function voteableAgeComparison()
      * variables can only Exist (to be used) within this block/function.
     */              
     let userAge;
-    let age;
+    let age = document.getElementById("age").value;
  
     /**
      * instruct the user to enter his/her age as an "int".
@@ -14,12 +15,12 @@ function voteableAgeComparison()
      */
      if ( isNaN( age ) )
      {
-         userAge = "Invalid Entry! ";
+         userAge = "Please Use Whole Numbers! ";
      }
      else
      {
         // Assign the user's "age" value to the variable "age". 
-         age = document.getElementById( "age" ).value;
+        //  age = document.getElementById( "age" ).value;
          /**
           * If, voteableAge, is under 18. Output String following 
           * the "question mark (?) char", is displayed. Else, 
@@ -30,24 +31,34 @@ function voteableAgeComparison()
  
      document.getElementById( "ageId" ).innerHTML = userAge + "To Vote!";
 } 
+//              ---- END_TERNARY_OPERATOR ----
 //              ---- END_VOTING_AGE_COMPARISON ----
-//              ---- KEYWORDS_VARIABLES_CONSTRUCTORS ----
-// Function and parameters
-function vehicleChoice( make, model, year, color )
-{
-    this.vehicleMake = make;
-    this.vehicleModel = model;
-    this.vehicleYear = year;
-    this.vehicleColor = color;
-}
 
-const Christina = new vehicleChoice( "Tesla", "Model_X Luxury SUV", 2024, "Black" );
-const Emily = new vehicleChoice( "Jeep", "Cherokee Trail Hawk", 2024, "White and Black" );
-const Khalil = new vehicleChoice( "Ford", "Hennessey F-150 VelociRaptor R_1000 4X4", 2024, 
+
+//              ---- CONSTRUCTOR ----
+//              ---- CAR_CLASS ----
+
+    function vehicle( make, model, year, color )
+    {
+        this.vehicleMake = make;
+        this.vehicleModel = model;
+        this.vehicleYear = year;
+        this.vehicleColor = color;
+    }   
+
+    var carOne = new vehicle( "Tesla", "Model_X Luxury SUV", 2024, "Black" );
+    var carTwo = new vehicle( "Jeep", "Cherokee Trail Hawk", 2024, "White and Black" );
+    var carThree = new vehicle( "Ford", "Hennessey F-150 VelociRaptor R_1000 4X4", 2024, 
 "Black" );
 
-    document.getElementById( "new_and_this" ).innerHTML = "Christina Drives a " + 
-    Christina.vehicleColor + " Colored, " + vehicleYear + ", " + vehicleModel; 
+    function vehicleChoice()
+    {
+        
+    document.getElementById( "new-this-const" ).innerHTML = "In my opinion the" + 
+    carThree.vehicleModel + " was the most Capable and the most powerfull,<br>truck on the"
+    + "planet. Not to mention it's Luxury and Intimidating appearance makes it even " + 
+    "more appealing. And now the " + carThree.vehicleYear + ", just got even better.";
+    } 
 //              ---- END_KEYWORDS_VARIABLES_CONSTRUCTORS ----
 //              ----END_VOTING_AGE_COMPARISON ----
 
@@ -56,19 +67,21 @@ const Khalil = new vehicleChoice( "Ford", "Hennessey F-150 VelociRaptor R_1000 4
 //              ---- NESTED_FUNCTION ----
 function functionCount()
 {
-    document.getElementById( "nested-function" ).innerHTML = count();
+
+    var startingPoint = 9;
 
     function count()
     {
-        var startingPoint = 9;
 
-        function plusOne() { starting = starting + 1; }
+        var result = startingPoint += 1;
 
-        plusOne();
+        document.getElementById( "nested-function" ).innerHTML = result;
 
-        return startingPoint;
 
     }
+
+    count();
+
 }
 
 
